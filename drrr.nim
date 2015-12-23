135dbc7a28313d7e5511c9e450bf4b38d2519d9b
encorehu <huyoo353@126.com>
2015-12-23 22:23:50 +0800

    add some routes

--- drrr.nim
+++ drrr.nim
@@ -5,5 +5,13 @@ import
 routes:
   get "/":
     resp h1("Hello world")
+  get "/lounge":
+    resp h1("Hello world")
+  get "/room":
+    resp h1("Hello world")
+  get "/create_room":
+    resp h1("Hello world")
+  get "/json":
+    resp h1("Hello world")
 
 runForever()
