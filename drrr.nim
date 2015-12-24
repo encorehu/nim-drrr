051b6165336bb8a89a543d8497a7ac48be6e14cd
encorehu <huyoo353@126.com>
2015-12-24 09:08:09 +0800

    clean

--- drrr.nim
+++ drrr.nim
@@ -36,7 +36,6 @@ routes:
     var data = request.formData["file"].body
     var timestamp = int(toSeconds(getTime()))
     writeFile("public/upfiles/" & $timestamp & ".jpg", data)
-    #resp(data, contentType="image/jpeg")
     redirect("/upfiles/" & $timestamp & ".jpg")
 
 runForever()
